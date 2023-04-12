902ee179ebcb312062f8ed151b1e5ced3a5b4981
Vadim Kostrov <vmkvo15@outlook.com>
2023-04-12 18:27:39 +0400

    Add tab functionality plus active styling

--- src/index.js
+++ src/index.js
@@ -3,10 +3,61 @@ import {default as homeContent} from './homeContent.js';
 import './style.css';
 
 const mainContainer = document.querySelector('#content');
-
 mainContainer.appendChild(initialPage());
-
 const contentContainer = document.querySelector('.pageContainer');
-
 contentContainer.appendChild(homeContent());
+
+const navBarLinks = document.querySelectorAll('.link');
+console.log(navBarLinks);
+
+function unActive() {
+
+    navBarLinks.forEach((link, index) => { navBarLinks[index].className = 'link'});
+
+    footBarLinks.forEach((link, index) => { footBarLinks[index].className = 'footLink'});
+
+}
+
+navBarLinks.forEach((link, index) => navBarLinks[index].addEventListener('click', (e) => {
+    switch(index) {
+        case 0:
+            unActive();
+            e.target.classList += ' active';
+            contentContainer.innerHTML = '';
+            contentContainer.appendChild(homeContent());
+            break;
+        case 1:
+            unActive();
+            e.target.classList += ' active';
+            contentContainer.innerHTML = '';
+            break;
+        case 2:
+            unActive();
+            e.target.classList += ' active';
+            contentContainer.innerHTML = '';
+            break;
+    };
+}));
+
+const footBarLinks = document.querySelectorAll('.footLink');
+
+footBarLinks.forEach((link, index) => footBarLinks[index].addEventListener('click', (e) => {
+    switch(index) {
+        case 0: 
+            unActive();
+            e.target.classList += ' active';
+            contentContainer.innerHTML = '';
+            break;
+        case 1:
+            unActive();
+            e.target.classList += ' active';
+            contentContainer.innerHTML = '';
+            break;
+        case 2:
+            unActive();
+            e.target.classList += ' active';
+            contentContainer.innerHTML = '';
+            break;
+    }
+}));
 
