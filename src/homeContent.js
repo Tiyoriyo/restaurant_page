5717b29c0dbfa962fb1720e3200bf320c9f407b1
Vadim Kostrov <vmkvo15@outlook.com>
2023-04-12 17:50:47 +0400

    Clean code

--- src/homeContent.js
+++ src/homeContent.js
@@ -5,46 +5,61 @@ export default function render() {
     const content = document.createElement('div');
     content.classList.add('pageContent');
     
-    const contentLeft = document.createElement('div');
-    contentLeft.classList.add('contentLeft');
-    const map = new Image();
-    map.src = Map;
-    map.classList.add('map');
+    function renderContent() {
+        function renderContentLeft() {
+            const contentLeft = document.createElement('div');
+            contentLeft.classList.add('contentLeft');
 
-    contentLeft.appendChild(map);
+            const map = new Image();
+            map.src = Map;
 
+            map.classList.add('map');
+        
+            contentLeft.appendChild(map);
+            content.append(contentLeft);
+        }
 
-    const contentRight = document.createElement('div');
-    contentRight.classList.add('contentRight');
+        function renderContentRight() {
+            const contentRight = document.createElement('div');
+            contentRight.classList.add('contentRight');
 
-    const contentRightTop = document.createElement('div');
-    const contentRightBottom = document.createElement('div');
-    contentRightTop.classList.add('contentRightTop');
-    contentRightBottom.classList.add('contentRightBottom');
+            const contentRightTop = document.createElement('div');
+            const contentRightBottom = document.createElement('div');
+            contentRightTop.classList.add('contentRightTop');
+            contentRightBottom.classList.add('contentRightBottom');
 
-    const title = document.createElement('h1');
-    const subtitle = document.createElement('h2');
-    const description = document.createElement('div');
-    title.classList.add('title');
-    subtitle.classList.add('subtitle');
-    description.classList.add('description');
-    
-    for (let i = 0; i < 3; i++) {
-        const paragraph = document.createElement('p');
-        paragraph.classList.add('descriptionPara');
-        paragraph.innerHTML = descriptor[i];
-        description.appendChild(paragraph);
+            const title = document.createElement('h1');
+            const subtitle = document.createElement('h2');
+            const description = document.createElement('div');
+            title.classList.add('title');
+            subtitle.classList.add('subtitle');
+            description.classList.add('description');
+
+            title.innerHTML = 'Gazelle Diner!';
+            subtitle.innerHTML = 'Experience the Taste of the Savannah at Gazelle Diner'
+            for (let i = 0; i < 3; i++) {
+                const paragraph = document.createElement('p');
+                paragraph.classList.add('descriptionPara');
+                paragraph.innerHTML = descriptor[i];
+                description.appendChild(paragraph);
+            }
+
+            
+            contentRightTop.append(title, subtitle);
+            contentRightBottom.append(description);
+
+            contentRight.append(contentRightTop, contentRightBottom);
+            
+            content.append(contentRight);
+        }
+
+        renderContentLeft();
+        renderContentRight();
+
     }
 
-    title.innerHTML = 'Gazelle Diner!';
-    subtitle.innerHTML = 'Experience the Taste of the Savannah at Gazelle Diner'
+    renderContent();
     
-    contentRightTop.append(title, subtitle);
-    contentRightBottom.append(description);
-
-    contentRight.append(contentRightTop, contentRightBottom);
-    
-    content.append(contentLeft, contentRight);
     return content
 }
 
