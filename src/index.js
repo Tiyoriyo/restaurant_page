9bdc5316d0ed656889a295ec5fc59ce8689e13c5
Vadim Kostrov <vmkvo15@outlook.com>
2023-04-12 14:33:34 +0400

    Add styling and footer content

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {default as homePage} from './homePage.js';
+import {default as initialPage} from './initialPageLoad.js';
 import './style.css';
 
-document.querySelector('#content').appendChild(homePage());
+document.querySelector('#content').appendChild(initialPage());
